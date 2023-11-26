897c795afbd43fa3be57d6e2e1d38d2fc5f77735
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-11-26 16:59:48 +0900

    feat: Update AppMain of receiver sample

--- samples/MocastStudio.Receiver.Unity/Assets/MocastStudio.Samples.Receiver/Scripts/Application/AppMainLifecycle.cs
+++ samples/MocastStudio.Receiver.Unity/Assets/MocastStudio.Samples.Receiver/Scripts/Application/AppMainLifecycle.cs
@@ -1,3 +1,7 @@
+using MocastStudio.Samples.Receiver.Domain.MotionActor;
+using MocastStudio.Samples.Receiver.Infrastructure.Constants;
+using MocastStudio.Samples.Receiver.Infrastructure.MotionActor;
+using MocastStudio.Samples.Receiver.UIView;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -6,10 +10,24 @@ namespace MocastStudio.Samples.Receiver.Application
 {
     public sealed class AppMainLifecycle : LifetimeScope
     {
+        [SerializeField] private MotionActorLoaderUIView _motionActorLoaderUIView;
+
         protected override void Configure(IContainerBuilder builder)
         {
             Debug.Log($"[{nameof(AppMainLifecycle)}] Configure");
+
             builder.RegisterEntryPoint<AppMain>(Lifetime.Singleton);
+            builder.RegisterEntryPoint<MotionActorLoaderPresenter>(Lifetime.Singleton);
+
+            builder.Register<MotionActorService>(Lifetime.Singleton);
+
+            builder.Register<MotionActorFactory>(Lifetime.Singleton).AsImplementedInterfaces();
+
+            builder.Register<VrmAvatarResourceProvider>(Lifetime.Singleton).AsImplementedInterfaces()
+                    .WithParameter<RenderPipelineType>(RenderPipelineType.UniversalRenderPipeline)
+                    .WithParameter<IBinaryDataProvider>(new LocalFileBinaryDataProvider());
+
+            builder.RegisterComponent(_motionActorLoaderUIView);
         }
     }
 }
