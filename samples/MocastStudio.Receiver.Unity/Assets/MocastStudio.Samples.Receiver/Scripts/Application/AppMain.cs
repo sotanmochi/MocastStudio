9b0f7962e459011dfd4e6feb2ec97355f5b15cdf
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-12-01 22:22:51 +0900

    feat: Update AppMain of receiver sample project

--- samples/MocastStudio.Receiver.Unity/Assets/MocastStudio.Samples.Receiver/Scripts/Application/AppMain.cs
+++ samples/MocastStudio.Receiver.Unity/Assets/MocastStudio.Samples.Receiver/Scripts/Application/AppMain.cs
@@ -1,3 +1,7 @@
+using System;
+using MessagePipe;
+using MocastStudio.Samples.Receiver.Domain.MotionActor;
+using MocastStudio.Samples.Receiver.Infrastructure.StreamingReceiver;
 using UnityEngine;
 using VContainer.Unity;
 
@@ -6,11 +10,40 @@ namespace MocastStudio.Samples.Receiver.Application
     /// <summary>
     /// Entry point
     /// </summary>
-    public sealed class AppMain : IInitializable
+    public sealed class AppMain : IInitializable, IDisposable
     {
+        private readonly MotionActorService _motionActorService;
+        private readonly HumanPoseStreamingReceiver _streamingReceiver;
+        private readonly ISubscriber<HumanPose> _humanPoseSubscriber;
+
+        private int _actorId;
+        private IDisposable _disposable;
+
+        public AppMain(
+            MotionActorService motionActorService,
+            HumanPoseStreamingReceiver streamingReceiver,
+            ISubscriber<HumanPose> humanPoseSubscriber)
+        {
+            _motionActorService = motionActorService;
+            _streamingReceiver = streamingReceiver;
+            _humanPoseSubscriber = humanPoseSubscriber;
+        }
+
+        public void Dispose()
+        {
+            _disposable?.Dispose();
+            _streamingReceiver.Stop();
+            Debug.Log($"[{nameof(AppMain)}] Disposed");
+        }
+
         void IInitializable.Initialize()
         {
-            Debug.Log($"[{nameof(AppMain)}] Initialize");
+            _streamingReceiver.Start();
+
+            _disposable = _humanPoseSubscriber.Subscribe(humanPose =>
+                _motionActorService.UpdateMotionActorPose(_actorId, ref humanPose));
+
+            Debug.Log($"[{nameof(AppMain)}] Initialized");
         }
     }
 }
--- samples/MocastStudio.Receiver.Unity/Assets/MocastStudio.Samples.Receiver/Scripts/Application/AppMainLifecycle.cs
+++ samples/MocastStudio.Receiver.Unity/Assets/MocastStudio.Samples.Receiver/Scripts/Application/AppMainLifecycle.cs
@@ -1,8 +1,11 @@
+using MessagePipe;
 using MocastStudio.Samples.Receiver.Domain.MotionActor;
 using MocastStudio.Samples.Receiver.Infrastructure.Constants;
 using MocastStudio.Samples.Receiver.Infrastructure.MotionActor;
+using MocastStudio.Samples.Receiver.Infrastructure.StreamingReceiver;
 using MocastStudio.Samples.Receiver.UIView;
 using UnityEngine;
+using uOSC;
 using VContainer;
 using VContainer.Unity;
 
@@ -11,10 +14,13 @@ namespace MocastStudio.Samples.Receiver.Application
     public sealed class AppMainLifecycle : LifetimeScope
     {
         [SerializeField] private MotionActorLoaderUIView _motionActorLoaderUIView;
+        [SerializeField] private uOscServer _oscServer;
 
         protected override void Configure(IContainerBuilder builder)
         {
             Debug.Log($"[{nameof(AppMainLifecycle)}] Configure");
+
+            ConfigureMessagePipe(builder);
 
             builder.RegisterEntryPoint<AppMain>(Lifetime.Singleton);
             builder.RegisterEntryPoint<MotionActorLoaderPresenter>(Lifetime.Singleton);
@@ -27,7 +33,21 @@ namespace MocastStudio.Samples.Receiver.Application
                     .WithParameter<RenderPipelineType>(RenderPipelineType.UniversalRenderPipeline)
                     .WithParameter<IBinaryDataProvider>(new LocalFileBinaryDataProvider());
 
+            builder.Register<HumanPoseStreamingReceiver>(Lifetime.Singleton);
+
+            builder.RegisterComponent(_oscServer);
             builder.RegisterComponent(_motionActorLoaderUIView);
+        }
+
+        private void ConfigureMessagePipe(IContainerBuilder builder)
+        {
+            var options = builder.RegisterMessagePipe(/* configure option */);
+
+            // Setup GlobalMessagePipe to enable diagnostics window and global function
+            builder.RegisterBuildCallback(c => GlobalMessagePipe.SetProvider(c.AsServiceProvider()));
+
+            // Register for IPublisher<T>/ISubscriber<T>, includes async and buffered.
+            builder.RegisterMessageBroker<HumanPose>(options);
         }
     }
 }
