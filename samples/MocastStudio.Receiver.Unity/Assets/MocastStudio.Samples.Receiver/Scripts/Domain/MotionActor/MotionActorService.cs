f240a1619cba29f9fca0d6ab286a5bc9873814f0
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-12-01 22:21:33 +0900

    feat: Update MotionActorService of receiver sample project

--- samples/MocastStudio.Receiver.Unity/Assets/MocastStudio.Samples.Receiver/Scripts/Domain/MotionActor/MotionActorService.cs
+++ samples/MocastStudio.Receiver.Unity/Assets/MocastStudio.Samples.Receiver/Scripts/Domain/MotionActor/MotionActorService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
+using UnityEngine;
 
 namespace MocastStudio.Samples.Receiver.Domain.MotionActor
 {
@@ -28,6 +29,12 @@ namespace MocastStudio.Samples.Receiver.Domain.MotionActor
         {
             var motionActor = await _motionActorFactory.CreateAsync(resourcePath, cancellationToken);
             _humanoidMotionActors.Add(motionActor);
+        }
+
+        public void UpdateMotionActorPose(int actorId, ref HumanPose humanPose)
+        {
+            if (actorId < 0 || actorId >= _humanoidMotionActors.Count) return;
+            _humanoidMotionActors[actorId].SetHumanPose(ref humanPose);
         }
     }
 }
