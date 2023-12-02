bffe7fd44c2d4cffa63b256a8ada813ae6f3befb
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-12-02 12:58:30 +0900

    feat: Add TextMeshProComponentExtensions

---
+++ src/MocastStudio.Unity/Assets/MocastStudio.Universal/UIView/Utility/TextMeshProComponentExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using TMPro;
+
+namespace UniRx
+{
+    public static partial class TextMeshProComponentExtensions
+    {
+        /// <summary>Observe onValueChanged with current `value` on subscribe.</summary>
+        public static IObservable<int> OnValueChangedAsObservable(this TMP_Dropdown dropdown)
+        {
+            return Observable.CreateWithState<int, TMP_Dropdown>(dropdown, (d, observer) =>
+            {
+                observer.OnNext(d.value);
+                return d.onValueChanged.AsObservable().Subscribe(observer);
+            });
+        }
+    }
+}
