16a1c28276e41342cc0865f45971938df2a59722
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-12-02 18:14:13 +0900

    feat: Update MotionDataSourceType

--- src/MocastStudio.Unity/Assets/MocapSignalTransmission.Infrastructure/Constants/MotionDataSourceType.cs
+++ src/MocastStudio.Unity/Assets/MocapSignalTransmission.Infrastructure/Constants/MotionDataSourceType.cs
@@ -2,6 +2,7 @@ namespace MocapSignalTransmission.Infrastructure.Constants
 {
     public enum MotionDataSourceType
     {
+        Unknown = 0,
         VMCProtocol_TypeA = 11,
         VMCProtocol_TypeB = 12,
         Mocopi = 20,
