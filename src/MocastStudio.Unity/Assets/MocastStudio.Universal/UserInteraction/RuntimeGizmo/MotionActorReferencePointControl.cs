647083e9b9c679b32e7fda8bca76c6a426497ac8
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-12-12 22:46:31 +0900

    feat: Add MotionActorReferencePointControl

---
+++ src/MocastStudio.Unity/Assets/MocastStudio.Universal/UserInteraction/RuntimeGizmo/MotionActorReferencePointControl.cs
@@ -0,0 +1,49 @@
+using System;
+using MessagePipe;
+using MocapSignalTransmission.MotionActor;
+using MocastStudio.CameraSystem;
+using TransformControl;
+using VContainer;
+using VContainer.Unity;
+using Camera = UnityEngine.Camera;
+
+namespace MocastStudio.Universal.UserInteraction.RuntimeGizmo
+{
+    public sealed class MotionActorReferencePointControl : IInitializable, IDisposable
+    {
+        readonly MotionActorServiceContext _motionActorServiceContext;
+        readonly CameraSystemContext _cameraSystemContext;
+
+        IDisposable _disposable;
+
+        public MotionActorReferencePointControl(
+            MotionActorServiceContext motionActorServiceContext,
+            CameraSystemContext cameraSystemContext)
+        {
+            _motionActorServiceContext = motionActorServiceContext;
+            _cameraSystemContext = cameraSystemContext;
+        }
+
+        void IDisposable.Dispose() => _disposable.Dispose();
+
+        void IInitializable.Initialize()
+        {
+            var disposableBag = DisposableBag.CreateBuilder();
+
+            _motionActorServiceContext.OnMotionActorAdded
+                .Subscribe(motionActor =>
+                {
+                    if (motionActor is HumanoidMotionActor humanoidMotionActor)
+                    {
+                        var control = humanoidMotionActor.BodyTrackingActorBehaviour.gameObject.AddComponent<TransformController>();
+                        control.renderTargetCamera = _cameraSystemContext.SceneViewCamera;
+                    }
+                })
+                .AddTo(disposableBag);
+
+            _disposable = disposableBag.Build();
+
+            UnityEngine.Debug.Log($"<color=lime>[{nameof(MotionActorReferencePointControl)}] Initialized</color>");
+        }
+    }
+}
