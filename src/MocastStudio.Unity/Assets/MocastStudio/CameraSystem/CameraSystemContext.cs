708b81124e66a5e4fec2e922272038447181b29d
sotanmochi <16936357+sotanmochi@users.noreply.github.com>
2023-12-12 22:44:47 +0900

    feat: Add CameraSystemContext

---
+++ src/MocastStudio.Unity/Assets/MocastStudio/CameraSystem/CameraSystemContext.cs
@@ -0,0 +1,9 @@
+using Camera = UnityEngine.Camera;
+
+namespace MocastStudio.CameraSystem
+{
+    public sealed class CameraSystemContext
+    {
+        public Camera SceneViewCamera { get; set; }
+    }
+}
